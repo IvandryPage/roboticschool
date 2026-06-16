c339535f4fb4c9b3f2206cbd1d10a32067ef27fe
alfurqan10 <muhammadalfurqan275@gmail.com>
2026-06-16 17:06:15 +0700

    Menambahkan fitur materi pembelajaran

--- app/Filament/Resources/MateriPembelajarans/Schemas/MateriPembelajaranForm.php
+++ app/Filament/Resources/MateriPembelajarans/Schemas/MateriPembelajaranForm.php
@@ -4,9 +4,6 @@ namespace App\Filament\Resources\MateriPembelajarans\Schemas;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
-<<<<<<< HEAD
-use Filament\Forms\Components\FileUpload;
-=======
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\FileUpload;
 use Filament\Schemas\Components\Section;
@@ -18,40 +15,6 @@ class MateriPembelajaranForm
     {
         return $schema
             ->components([
-<<<<<<< HEAD
-                TextInput::make('sesi_id')
-                    ->label('ID Sesi')
-                    ->required(),
-
-                TextInput::make('judul')
-                    ->label('Judul Materi')
-                    ->required()
-                    ->maxLength(255),
-
-                TextInput::make('urutan')
-                    ->label('Nomor Urut')
-                    ->numeric()
-                    ->required(),
-
-                FileUpload::make('file_path_atau_url') // Pastikan nama ini sama dengan kolom di database
-                    ->label('Unggah File Materi')
-                    ->directory('materi_pembelajaran')
-                    ->acceptedFileTypes([
-                        'application/pdf', 
-                        'image/jpeg', 
-                        'image/png', 
-                        'application/msword', 
-                        'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
-                    ])
-                    ->rules(['mimes:pdf,jpg,jpeg,png,doc,docx'])
-                    ->helperText('Format yang diizinkan: PDF, Gambar (JPG/PNG), dan Word (DOC/DOCX).')
-                    ->required(),
-
-                Textarea::make('keterangan')
-                    ->label('Keterangan')
-                    ->columnSpanFull(),
-=======
-                Section::make('Informasi Materi')
                 Section::make('Informasi Materi Pembelajaran')
                     ->schema([
                         TextInput::make('sesi_id')
@@ -97,7 +60,6 @@ class MateriPembelajaranForm
                             ->label('Keterangan Singkat')
                             ->columnSpanFull(),
                     ]),
->>>>>>> 01e1427be3bd9d2e5adbe5a70f2b7ec8f39b390d
             ]);
     }
 }
