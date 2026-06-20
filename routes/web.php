6ee8fd21f57e81d405d1d077a0a7ace0156acd67
syahrul-3 <124240005@student.upnyk.ac.id>
2026-06-20 20:00:19 +0700

    sudah kredensial dan manajemen session pada PBi-010, dan pengalihan otomatis ke halaman dashboard sesuai peran masing-masing setelah login berhasil yang contohnya http://127.0.0.1:8000/dashboard menjadi http://127.0.0.1:8000/admin/dashboard

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,11 @@ Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::view('/admin/dashboard', 'dashboard');
+    Route::view('/instruktur/dashboard', 'dashboard');
+    Route::view('/siswa/dashboard', 'dashboard');
+    Route::view('/publikasi/dashboard', 'dashboard');
+    Route::view('/direktur/dashboard', 'dashboard');
     #buat jalur ke UserController
     Route::prefix('admin')->name('admin.')->group(function () {
         Route::resource('users', UserController::class);
