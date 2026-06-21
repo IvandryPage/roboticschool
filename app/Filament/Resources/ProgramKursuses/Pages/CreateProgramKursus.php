901fd888f4a4aebfaa75dbd31e407f29c74daa67
rahzaame <rahzaame@gmail.com>
2026-06-21 16:51:43 +0700

    PB-03 implement CRUD Program Kursus dan Materi Program

---
+++ app/Filament/Resources/ProgramKursuses/Pages/CreateProgramKursus.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\ProgramKursuses\Pages;
+
+use App\Filament\Resources\ProgramKursuses\ProgramKursusResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateProgramKursus extends CreateRecord
+{
+    protected static string $resource = ProgramKursusResource::class;
+}
