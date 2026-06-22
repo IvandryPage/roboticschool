1908b5322c94c63e4c4ff8d07c596e2ca287549b
rahzaame <rahzaame@gmail.com>
2026-06-22 11:53:18 +0700

    menghapus tombol create & create another, membenahi delete

--- app/Filament/Resources/ProgramKursuses/Pages/CreateProgramKursus.php
+++ app/Filament/Resources/ProgramKursuses/Pages/CreateProgramKursus.php
@@ -3,9 +3,18 @@
 namespace App\Filament\Resources\ProgramKursuses\Pages;
 
 use App\Filament\Resources\ProgramKursuses\ProgramKursusResource;
+use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateProgramKursus extends CreateRecord
 {
     protected static string $resource = ProgramKursusResource::class;
+
+    protected function getFormActions(): array
+    {
+        return [
+            $this->getCreateFormAction(),
+            $this->getCancelFormAction(),
+        ];
+    }
 }
